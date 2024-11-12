8820b80874f286d881467879832614bd06b88759
Swayam Gupta <swayamg741@gmail.com>
2024-11-13 04:53:46 +0530

    registeration-api-userController.js

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,3 +1,14 @@
+import userModel from "../models/userModel.js";
+import bcrypt from "bcrypt"
+import validator from "validator";
+import jwt from "jsonwebtoken"
+
+
+
+
+const createToken = (id) => {
+  return jwt.sign({id}, process.env.JWT_SECRET)
+}
 
 
 const loginUser=async (req, res) => {
@@ -6,8 +17,47 @@ const loginUser=async (req, res) => {
 
 const registerUser=async (req, res) => {
   
+    try
+    {
+        const {name, email, password}=req.body;
+        const exists=await userModel.findOne({email});
+        if(exists)
+        {
+            return res.json({success: false, message:"User exists"})
 
-    res.json({msg: "Register"})
+        }
+
+        if(!validator.isEmail(email))
+        {
+            return res.json({success:false, message: "Enter valid email"})
+        }
+
+        if(password.length<8)
+        {
+            return res.json({success: false, message: "Enter strong password"})
+        }
+        
+        const salt=await bcrypt.genSalt(10)
+        const hashedPassword=await bcrypt.hash(password, salt)
+
+        const newUser=new userModel({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        const user=await newUser.save();
+
+
+        const token=createToken(user._id)
+
+        res.json({success: true, token})
+    }
+    catch(error)
+    {
+        console.log(error);
+        res.json({success: false, message: error.message})
+    }
 }
 
 const adminLogin= async (req, res) => {
