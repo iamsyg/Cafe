66dffdba3582daae8894420d04c064098ea312c5
Swayam Gupta <swayamg741@gmail.com>
2024-11-13 07:22:53 +0530

    images-cloudinary

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,4 +1,4 @@
-
+import { v2 as cloudiary } from "cloudinary";
 
 const addProduct = async (req, res) => {
   
@@ -9,8 +9,18 @@ const addProduct = async (req, res) => {
 
         const image1= req.files.image1 && req.files.image1[0];
 
+        const images=[image1].filter((item)=>item!==undefined)
+
+        let imageUrl=await Promise.all(
+            images.map(async (item)=>{
+                 
+                let result=await cloudiary.uploader.upload(item.path, {resource_type:"image"});
+                return result.secure_url;
+            })
+        )
+
         console.log(name, description, price, category)
-        console.log(image1);
+        console.log(imageUrl);
 
         res.json({})
     } catch (error) {
