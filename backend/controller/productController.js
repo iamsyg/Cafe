146a732047c8fc931c521b8d00b79e2188404977
Swayam Gupta <swayamg741@gmail.com>
2024-11-13 06:05:01 +0530

    product-api

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -2,6 +2,22 @@
 
 const addProduct = async (req, res) => {
   
+
+    try {
+        
+        const {name, description, price, category}=req.body;
+
+        const image1= req.files.image1 && req.files.image1[0];
+
+        console.log(name, description, price, category)
+        console.log(image1);
+
+        res.json({})
+    } catch (error) {
+        
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
 }
 
 
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 import {listProducts, addProduct, removeProduct} from "../controller/productController.js"
+import upload from "../middleware/multer.js";
 
 
 
 const productRouter=express.Router();
 
-productRouter.post("/add", addProduct)
+productRouter.post("/add", upload.fields([{name: "image1", maxCount: 1}]), addProduct)
 productRouter.post("/remove", removeProduct)
 productRouter.post("/list", listProducts);
 
