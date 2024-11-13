ffdb89ca5738dbdbc530a0edea0c9b5d8f6d7390
Tejashwani <tejashwanidixit77@gmail.com>
2024-11-13 13:45:44 +0530

    Admin-Panel

---
+++ src/admin/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {assets} from '../assets'
+
+const  = () => {
+  return (
+    <div className='flex items-center'>
+        <img  src={assets.logo} alt="" />
+        <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full'>LogOut</button>
+    </div>
+  )
+}
+
+export default 
