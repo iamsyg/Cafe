ab593877cbefb082c081b932b8c1cbb056c26f6f
Swayam Gupta <swayamg741@gmail.com>
2024-11-13 08:08:39 +0530

    single-product

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -9,6 +9,7 @@ const productRouter=express.Router();
 productRouter.post("/add", upload.fields([{name: "image1", maxCount: 1}]), addProduct)
 productRouter.post("/remove", removeProduct)
 productRouter.post("/list", listProducts);
+productRouter.post("/single", singleProduct);
 
 
 export default productRouter
